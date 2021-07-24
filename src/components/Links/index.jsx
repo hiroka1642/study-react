ef5f3dd5e2aa627b389df1d58152c7be68b7fe61
katan4722 <uver7770422@gmail.com>
2021-07-24 11:00:28 +0900

    ✨ コンポーネント間のStateのリフトアップについて学びました

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,42 +1,16 @@
-import classes from 'src/components/Links/Links.module.css'
+import classes from "src/components/Links/Links.module.css";
 
-const ITEMS=[
-  { 
-    href: "https://nextjs.org/docs" ,
-  title: "Documentation →",
-  discription: " Find in-depth information about Next.js features and API.",
-  },
-  { 
-    href: "https://nextjs.org/learn" ,
-  title: "Learn →",
-  discription: "Learn about Next.js in an interactive course with quizzes!",
-  },
-  { 
-    href: "https://github.com/vercel/next.js/tree/master/examples" ,
-  title: "Examples →",
-  discription: " Discover and deploy boilerplate example Next.js projects.",
-  },
-  { 
-    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" ,
-  title: "Deploy →",
-  discription: " Instantly deploy your Next.js site to a public URL with Vercel.",
-  }
-
-]
-
-export function Links() {
+export function Links({ handleReduce, items }) {
   return (
-
-        <div className={classes.grid}>
-          {ITEMS.map(item=>{
-            return (
-              <a key={item.href} href={item.href}className={classes.card}>
-              <h3>{item.title}</h3>
-              <p>{item.discription}</p>
-            </a>
-              )
-          })}
-        </div>
-
-  )
+    <div className={classes.grid}>
+      {items.map((item) => {
+        return (
+          <a key={item.href} href={item.href} className={classes.card}>
+            <h3>{item.title}</h3>
+            <p>{item.discription}</p>
+          </a>
+        );
+      })}
+    </div>
+  );
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,15 +1,47 @@
 import classes from "src/components/Main/Main.module.css";
 import { Headline } from "src/components/Headline";
 import { Links } from "src/components/Links";
+import { useCallback, useState } from "react";
+
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Documentation →",
+    discription: " Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn →",
+    discription: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://github.com/vercel/next.js/tree/master/examples",
+    title: "Examples →",
+    discription: " Discover and deploy boilerplate example Next.js projects.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy →",
+    discription:
+      " Instantly deploy your Next.js site to a public URL with Vercel.",
+  },
+];
 
 export default function Main(props) {
+  const [items, setItems] = useState(ITEMS);
+  const handleReduce = useCallback(() => {
+    setItems((prevItems) => {
+      return prevItems.slice(0, prevItems.length - 1);
+    });
+  }, []);
+
   return (
     <main className={classes.main}>
-      <Headline page={props.page}>
+      <Headline page={props.page} items={items} handleReduce={handleReduce}>
         <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
 
-      <Links />
+      <Links items={items}  />
     </main>
   );
 }
