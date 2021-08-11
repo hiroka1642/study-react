a7e3edd7c018bc3595900fdb6d8c4f6b59a9ed72
katan4722 <uver7770422@gmail.com>
2021-08-12 00:18:00 +0900

    🔥 Indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,37 +1,21 @@
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
-import Main from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 import { useBgColor } from "src/hooks/useBgColor";
 
-export default function About(props) {
+const Index = () => {
   useBgColor();
 
   return (
     <div className={styles.container}>
-      <Header />
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button href="/about" onClick={props.handleclick}>
-        ボタン
-      </button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main page="about" />
-
-      <Footer />
+      <Header />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONPlaceholderのAPIをいろいろ叩いてみるよ！</p>
     </div>
   );
-}
+};
+
+export default Index;
