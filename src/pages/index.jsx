1e732afaffcc24578a6e80cc82d17426bae065b1
katan4722 <uver7770422@gmail.com>
2021-08-12 00:13:22 +0900

    🚚 AboutのパスをIndexに変更。リファクタリング

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,17 +5,7 @@ import Main from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 import { useBgColor } from "src/hooks/useBgColor";
 
-export default function About({
-  count,
-  isShow,
-  handleclick,
-  handleDisplay,
-  doubleCount,
-  text,
-  array,
-  handleAdd,
-  handleChange,
-}) {
+export default function About(props) {
   useBgColor();
 
   return (
@@ -24,15 +14,17 @@ export default function About({
       <Head>
         <title>About Page</title>
       </Head>
-      {isShow ? <h1>{doubleCount}</h1> : null}
-      <button href="/about" onClick={handleclick}>
+      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
+      <button href="/about" onClick={props.handleclick}>
         ボタン
       </button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={handleAdd}>追加</button>
+      <button onClick={props.handleDisplay}>
+        {props.isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={props.text} onChange={props.handleChange} />
+      <button onClick={props.handleAdd}>追加</button>
       <ul>
-        {array.map((item) => {
+        {props.array.map((item) => {
           return <li key={item}>{item}</li>;
         })}
       </ul>
