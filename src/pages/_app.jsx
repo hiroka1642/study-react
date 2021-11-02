561f776a7a837065f07b4644f57e5acf50e3cdf6
katan4722 <uver7770422@gmail.com>
2021-11-02 22:38:14 +0900

    👽️ SWRのv1対応

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,17 +2,24 @@
 import "tailwindcss/tailwind.css";
 import Head from "next/head";
 import { Layout } from "src/components/Layout/Index";
+import { SWRConfig } from "swr";
 
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+  const json = await res.json();
+  return json;
+};
 const MyApp = (props) => {
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Layout>
-        <props.Component {...props.pageProps} />
-      </Layout>
+      <SWRConfig value={{ fetcher }}>
+        <Layout>
+          <props.Component {...props.pageProps} />
+        </Layout>
+      </SWRConfig>
     </>
   );
 };
