e511151025e97ddd359fbc729311b6308ebae2de
katan4722 <uver7770422@gmail.com>
2021-11-06 18:29:47 +0900

    ⚡️ コメント一覧のページをSG化

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,15 +1,35 @@
 import Head from "next/head";
 import { Header } from "src/components/Header";
 import { Comments as CommentsComponent } from "src/components/Comments";
+import { SWRConfig } from "swr";
 
-const Comments = () => {
+// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const getStaticProps = async () => {
+  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+  const comment = await fetch(COMMENTS_API_URL);
+  const userData = await comment.json();
+  // await sleep(2000);
+  return {
+    props: {
+      fallback: {
+        [COMMENTS_API_URL]: userData,
+      },
+    },
+  };
+};
+
+const Comments = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Head>
         <title>Comments Page</title>
       </Head>
-      <Header />
-      <CommentsComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <CommentsComponent />
+      </SWRConfig>
     </div>
   );
 };
